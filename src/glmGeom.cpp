b2ba5dcb5be73176314b32f18a7c01806132e7b6
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-11-13 16:56:30 -0500

    tesselation and IO

--- src/glmGeom.cpp
+++ src/glmGeom.cpp
@@ -82,14 +82,10 @@ glm::vec3 getCentroid(const std::vector<glm::vec3> &_pts){
     return centroid;
 }
 
-std::vector<glm::vec3> getSimplify(const std::vector<glm::vec3> &_pts){
-    
-}
-
-bool lexicalComparison(const glm::vec3& v1, const glm::vec3& v2) {
-    if (v1.x > v2.x) return true;
-    else if (v1.x < v2.x) return false;
-    else if (v1.y > v2.y) return true;
+bool lexicalComparison(const glm::vec3 &_v1, const glm::vec3 &_v2) {
+    if (_v1.x > _v2.x) return true;
+    else if (_v1.x < _v2.x) return false;
+    else if (_v1.y > _v2.y) return true;
     else return false;
 }
 
@@ -122,7 +118,7 @@ std::vector<glm::vec3> getConvexHull(std::vector<glm::vec3> &pts){
     int currentPoint = 2;
     int direction = 1;
     
-    for (int i=0; i<2000; i++) { //max 1000 tries
+    for (int i=0; i<3000; i++) { //max 1000 tries
         
         hull.push_back(pts.at(currentPoint));
         
@@ -290,6 +286,4 @@ void simplify(std::vector<glm::vec3> &_pts, float _tolerance){
     }else{
         _pts = sV;
     }
-    
-    
 }
